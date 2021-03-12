9314fbb1491633e960578d46c179b32b2e4f67b2
Akshat Sahu <akshatsahu1@gmail.com>
2021-03-12 19:57:26 +0530

    Tic Tac Toe

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.tictactoe;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.service.dreams.DreamService;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button[][] buttons = new Button[3][3];
+    private boolean player1Turn = true;  //player with x will start first //
+    private int roundCount; //total round count will be 9//
+    private int player1Points;
+    private int player2Points;
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) { //checks if the button clicked has an empty string and if this is not the case this means that button has been already used. //
+            return;
+        }
+        if (player1Turn) {
+            ((Button) v).setText("X");
+        } else {
+            ((Button) v).setText("O");
+        }
+        roundCount++;
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn; //switch turns //
+        }
+    }
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+        for (int i = 0; i < 3; i++) {  //for all 3 rows //
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2]) //responsible for comparing the 3 fields next to each other//
+                    && !field[i][0].equals("")) {  //to makes sure its not just 3 types of empty fields//
+                return true;
+            }
+        }
+        for (int i = 0; i < 3; i++) { //for all 3 collumns //
+            if (field[0][i].equals(field[1][i]) //responsible for comparing the 3 fields next to each other//
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) { //to makes sure its not just 3 types of empty fields//
+                return true;
+            }
+        }
+        if (field[0][0].equals(field[1][1]) //Top LEFT to  Bottom RIGHT diagonal //
+                && field[0][0].equals(field[2][2]) //responsible for comparing the 3 fields next to each other//
+                && !field[0][0].equals("")) {  //to makes sure its not just 3 types of empty fields//
+            return true;
+        }
+        if (field[0][2].equals(field[1][1]) //Top RIGHT to  Bottom LEFT diagonal //
+                && field[0][2].equals(field[2][0]) //responsible for comparing the 3 fields next to each other//
+                && !field[0][2].equals("")) { //to makes sure its not just 3 types of empty fields//
+            return true;
+        }
+        return false;
+    }
+    private void player1Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+    private void player2Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+    private void draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+    private void updatePointsText() {
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        roundCount = 0;
+        player1Turn = true;
+    }
+    private void resetGame(){
+        player1Points=0;
+        player2Points=0;
+        updatePointsText();
+        resetBoard();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+    }
+}
